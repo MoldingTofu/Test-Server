69ab73057b09894740c112b1b03c8b45c0c39c14
Jeremy <chang489@purdue.edu>
2018-03-03 16:01:46 -0500

    lesson 2 server.js

--- server.js
+++ server.js
@@ -1,71 +1,24 @@
-var sqlite = require("sqlite3");
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var db = require("./database.js");
+db.run("CREATE TABLE IF NOT EXISTS groceries(" +
+    "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+    "name varchar(50) NOT NULL," +
+    "price varchar(50) NOT NULL," +
+    "type varchar(50) NOT NULL);",
+    function(err) {
+        if (err)
+            throw err;
+        console.log("Created groceries list if it didn't exist already")
+    });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var query = "INSERT INTO groceries(name, price) VALUES (\"Ian\", \"$20.45\");"
 
-app.use(function(req, res, next) { // request, response, next
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-   res.header("Access-Control-Allow-Headers", "Content-Type, cache-control");
-   return next();
-});
+/*
+setTimeout(function() {
+    db.all("SELECT * FROM groceries WHERE type = ?;", "Bean", function(err, users) {
+        if (err)
+            throw err;
 
-var port = process.env.PORT || 8080; // set our port
-var router = express.Router(); // get an instance of the express Router
+        console.log(users)
+    });
+}, 200);
+*/
 
-// middleware to use for all requests ------------------------------------------
-router.use(function(req, res, next) {
-   // do logging
-   console.log("Server in use");
-   next(); // make sure we go to the next routes and don't stop here
-});
-
-// this is an alias, so people don’t know the real file-path of your application
-app.use('/static', express.static(__dirname + '/app/webpage/'));
-//they can ask for `static/critical.js` and get `app/important/critical.js`, but
-//a hacker wouldn’t know the file path to actually get the file if they got mischievous
-
-router.route("/put/")
-    .get(function(req, res) { // GET requests respond with Hello World
-db.run("INSERT INTO users(email, pswd) VALUES (?,?);", ["him", "1234"], function(err) {
-   if (err) {
-       console.error(err.code);
-   } else {
-       console.log("making user...");
-       console.log({ message: "Success" });
-   }
-   return;
-});
-});
-
-router.route("/something/arbitrary/")
-    .get(function(req, res) { // GET requests respond with Hello World
-        // res.send(err.message);
-        res.json("{message: 'Hello World!'}");     // Turns structures into a string
-            // JavaScript Object Notation
-            // Can be parsed out by more than JavaScript (Python, etc)
-});
-
-router.route("/get/")
-    .get(function(req, res) { // GET requests respond with Hello World
-    query = "SELECT id FROM users WHERE email = ? AND pswd = ?;"
-db.all(query, ["me", "1234"], function(err, users) {
-           if (err)
-               console.log(err.message);
-           res.send(users);
-           return;
-       });
-});
-
-
-// -----------------------------------------------------------------------------
-//add-on to the IP address and port #, for minor security and/or personal flair
-app.use("/api", router);
-
-//Tell the application to listen on the port # you specified above
-app.listen(port);
-console.log("Express server listening on port %d in %s mode. ", port, app.settings.env)
