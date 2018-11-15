49dcc63294c92222cdd312f504e432c181b9ee9a
moldingtofu <jeremykchang@gmail.com>
2018-11-14 19:33:13 -0500

    small server?

---
+++ rov-socket/server.py
@@ -0,0 +1,20 @@
+import socket
+
+s = socket.socket()
+print "socket created"
+
+port = 8000
+
+s.bind(('', port))
+print "socket binded to %s" %(port)
+
+s.listen(5)
+print "socket is listening"
+
+while True:
+  c, addr = s.accept()
+  print "got connection from %s" %(addr)
+
+  c.send("sent")
+
+  c.close()
