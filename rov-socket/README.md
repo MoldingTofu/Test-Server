c0c59abe71bfd23e277bbd20ec44e384d07c27bc
moldingtofu <jeremykchang@gmail.com>
2018-11-28 17:00:39 -0500

    set up virtual environment

--- rov-socket/README.md
+++
@@ -1,4 +0,0 @@
-https://github.com/miguelgrinberg/Flask-SocketIO
-https://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
-https://docs.python-guide.org/dev/virtualenvs/
-https://python-socketio.readthedocs.io/en/latest/
--- rov-socket/mux.py
+++ rov-socket/mux.py
@@ -1,18 +1,23 @@
+#!/usr/bin/env python
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'key'
+app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-@socketio.on('message')
-def handle_message(message):
-  send(message)
+@app.route('/')
+def index():
+  return render_template('index.html')
 
-@socketio.on('json')
-def handle_json(json):
-  #send to ros?
-  print('received json: ' + str(json))
+@socketio.on('connect')
+def connect(message):
+  emit('reponse', {'data': 'data'})
+  print('connected')
+
+@socketio.on('disconnect')
+def disconnect():
+  print('disconnected')
 
 if __name__ == '__main__':
   socketio.run(app)
