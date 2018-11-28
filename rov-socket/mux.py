b7495ed171365b20f01bd6bdd052baf7e317cb98
moldingtofu <jeremykchang@gmail.com>
2018-11-28 18:58:50 -0500

    disconnect?

--- rov-socket/mux.py
+++ rov-socket/mux.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from threading import Lock
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, disconnect
 
 async_mode = None
 
@@ -11,21 +11,37 @@ socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
 
+def background_thread():
+  count = 0
+  while True:
+    socketio.sleep(10)
+    count += 1
+    socketio.emit('my_response', {'data': 'Server generated event', 'count': count}, namespace='/test')
+
 @app.route('/')
 def index():
   return render_template('index.html', async_mode=socketio.async_mode)
 
+@socketio.on('my_event', namespace='/test')
+def text_message(message):
+  session['receive_count'] = session.get('receive_count', 0) + 1
+  emit('my_response',
+    {'data': message['data'], 'count':session['receive_count']})
+
 @socketio.on('connect', namespace='/test')
-def connect():
+def test_connect():
   global thread
   with thread_lock:
     if thread is None:
       thread = socketio.start_background_task(target=background_thread)
   emit('my_response', {'data': 'Connected', 'count':0})
 
-@socketio.on('disconnect', namespace='/test')
-def disconnect():
-  print('disconnected', request.sid)
+@socketio.on('disconnect_request', namespace='/test')
+def disconnect_request():
+  session['receive_count'] = session.get('receive_count', 0) + 1
+  emit('my_response',
+    {'data': 'disconnected', 'count': session['receive_count']})
+  disconnect()
 
 if __name__ == '__main__':
   socketio.run(app)
