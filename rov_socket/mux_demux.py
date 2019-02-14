4d247be390a33f740bb05694ad6964017a4feb4a
moldingtofu <jeremykchang@gmail.com>
2019-02-13 20:18:08 -0500

    untested finished

--- rov_socket/mux_demux.py
+++ rov_socket/mux_demux.py
@@ -19,25 +19,20 @@ thread_lock = Lock()
 with open ('../../../../surface/frontend/src/packets.json') as json_data:
   data = json.load(json_data,)
 
-dearflask = data['dearflask']
-dearclient = data['dearclient']
-fmapper = packet_mapper(dearflask)
-cmapper = packet_mapper(dearclient)
+dearFlask = data['dearflask']
+dearClient = data['dearclient']
+flaskMapper = packet_mapper(dearFlask)
+clientMapper = packet_mapper(dearClient)
 thrust_pub = None
 auto_pub = None
 
 @socketio.on('dearflask')
 def dearflask(json):
-<<<<<<< HEAD
   dearFlask = json
   emit('my_response', dearClient)
 
   for data in dearFlask["thrusters"]:
     setattr(thrust_command_msg(), data, dearFlask["thrusters"][data])
-=======
-  global dearflask = json
-  emit('my_response', dearclient)
->>>>>>> 849dff9890afd5ee191629b4df37a9ea42476478
   thrust_pub.publish(thrust_command_msg())
 
   for data in dearFlask["auto"]:
