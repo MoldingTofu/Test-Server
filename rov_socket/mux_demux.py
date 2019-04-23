61575b6bba8fad0fae907d5cd2e435de0dbc5f68
Jeremy Chang <jeremykchang@gmail.com>
2019-04-23 18:36:22 -0400

    updated to current mux demux

--- rov_socket/mux_demux.py
+++ rov_socket/mux_demux.py
@@ -1,15 +1,34 @@
 #! /usr/bin/python
 import rospy
 import json
-import socket
+import socketio
+import engineio
+import eventlet
 import packet_mapper
-from shared_msgs.msg import can_msg, auto_command_msg, thrust_status_msg, thrust_command_msg, esc_single_msg
+import sys
+import copy
+import os
+#from threading import Thread, Lock
+import thread
+from shared_msgs.msg import can_msg, auto_command_msg, thrust_status_msg, thrust_command_msg, esc_single_msg, tools_command_msg
 from sensor_msgs.msg import Imu, Temperature
 from std_msgs.msg import Float32
-from StringIO import StringIO
 
-with open ('../../../../surface/frontend/src/packets.json') as json_data:
-  data = json.load(json_data,)
+print os.getcwd()
+
+try:
+  with open ('../../surface/frontend/src/packets.json') as json_data:
+    data = json.load(json_data,)
+except:
+  try:
+    with open ('../../../../surface/frontend/src/packets.json') as json_data:
+      data = json.load(json_data,)
+  except:
+    with open ('../X11-Core/surface/frontend/src/packets.json') as json_data:
+      data = json.load(json_data,)
+
+  
+
 
 dearflask = data['dearflask']
 dearclient = data['dearclient']
@@ -17,110 +36,105 @@ flask_mapper = packet_mapper.packet_mapper(dearflask)
 client_mapper = packet_mapper.packet_mapper(dearclient)
 thrust_pub = None
 auto_pub = None
-serversocket = None
-clientsocket = None
+tools_pub = None
 
-def serialize(data):
-  io = StringIO()
-  json.dump(data, io)
-  return io.getvalue()
+lock = thread.allocate_lock()
+kill = None
 
-def deserialize(data):
-  io = StringIO(data)
-  return json.load(io)
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+  '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
 
-def init_server():
-  global serversocket
-  serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  serversocket.bind((socket.gethostname(), 8000))
-  print('server started')
-  serversocket.listen(5)
 
-def listen():
-  global serversocket
-  global clientsocket
+@sio.on('dearRos')
+def accept(sid, data):
+    global dearflask
+    global dearclient
 
-  #listen for client
-  (clientsocket, address) = serversocket.accept()
-  print('client connected')
+    if not kill:
+        lock.acquire()
 
-def accept():
-  global serversocket
-  global clientsocket
-  global dearflask
-  global dearclient
+        dearflask = copy.deepcopy(data)
 
-  length = clientsocket.recv(10)
+        #pass back dearclient
+        # print dearclient
+        sio.emit('dearclient-response', dearclient)
 
-  try:
-    length = int(length)
-  except:
-    raise Exception()
+        # print data
+        #update thrust and auto
+        thrust = thrust_command_msg()
+        auto = auto_command_msg()
+        tools = tools_command_msg()
+        flask_mapper.pam(thrust, dearflask)
+        flask_mapper.pam(auto, dearflask)
+        flask_mapper.pam(tools, dearflask)
+        # print("Publishing:\n{}".format(thrust))
+        thrust_pub.publish(thrust)
+        auto_pub.publish(auto)
+        tools_pub.publish(tools)
 
-  dearflask = deserialize(clientsocket.recv(length))
+        lock.release()
+  
+    else:
+        raise eventlet.StopServe
 
-  #pass back dearclient
-  encode = serialize(dearclient)
-  head = str(len(encode))
-
-  for x in range(10 - len(head)):
-    head = '0' + head
-
-  encode = head + encode
-  clientsocket.send(encode)
-
-  #update thrust and auto
-  #flask_mapper.pam(thrust_command_msg(), dearflask)
-  #flask_mapper.pam(auto_command_msg(), dearflask)
-  #thrust_pub.publish(thrust_command_msg())
-  #auto_pub.publish(auto_command_msg())
-
-  return dearflask
+#  return dearflask
 
 def name_received(msg):
-  names = client_mapper.get_msg_vars(msg)
-  for name in names:
-    client_mapper.map(name, getattr(msg, name), dearclient)
+    global dearclient
+
+    if not kill:
+        lock.acquire()
+
+        names = client_mapper.get_msg_vars(msg)
+        for name in names:
+            client_mapper.map(name, getattr(msg, name), dearclient)
+        print(dearclient)
+
+        lock.release()
+
+def start_server():
+    global app
+    eventlet.wsgi.server(eventlet.listen(('', 5001)), app)
 
 if __name__ == "__main__":
-  global clientsocket
+    rospy.init_node('mux_demux')
+    ns = rospy.get_namespace() # This should return /surface
+    
+    # Retrieve data from the ROS System
+    esc_sub = rospy.Subscriber('/rov/esc_single',
+        esc_single_msg, name_received)
 
-  rospy.init_node('mux_demux')
-  ns = rospy.get_namespace() # This should return /surface
-  
-  # Retrieve data from the ROS System
-  esc_sub = rospy.Subscriber('/rov/esc_single', 
-      esc_single_msg, name_received)
+    status_sub = rospy.Subscriber('/rov/thrust_status', thrust_status_msg,
+        name_received);
 
-  status_sub = rospy.Subscriber('/rov/thrust_status', thrust_status_msg,
-    name_received);
+    temp_sub = rospy.Subscriber('/rov/temp', Temperature,
+        name_received);
 
-  temp_sub = rospy.Subscriber('/rov/temp', Temperature,
-    name_received);
+    imu_sub = rospy.Subscriber('/rov/imu', Imu,
+        name_received);
 
-  imu_sub = rospy.Subscriber('/rov/imu', Imu,
-    name_received);
+    ph_sub = rospy.Subscriber('/rov/ph', Float32,
+        name_received);
 
-  ph_sub = rospy.Subscriber('/rov/ph',Float32,
-    name_received);
+    depth_sub = rospy.Subscriber('/rov/depth', Float32,
+        name_received);
 
-  depth_sub = rospy.Subscriber('/rov/depth', Float32,
-    name_received);
+    # Publishers out onto the ROS System
+    thrust_pub = rospy.Publisher(ns + 'thrust_command',
+        thrust_command_msg, queue_size=10)
 
-  # Publishers out onto the ROS System
-  thrust_pub = rospy.Publisher(ns + 'thrust_command',
-    thrust_command_msg, queue_size=10);
+    auto_pub = rospy.Publisher(ns +'auto_command',
+        auto_command_msg, queue_size=10)
 
-  auto_pub = rospy.Publisher(ns +'auto_command',
-    auto_command_msg, queue_size=10);
+    tools_pub = rospy.Publisher(ns + 'tools_command',
+        tools_command_msg, queue_size=10)
 
-  init_server()
-  rate = rospy.Rate(10)
-  while not rospy.is_shutdown():
-    try:
-      data = accept()
-      print data
-    except:
-      print('no client connected')
-      listen()
-    rate.sleep()
+    t = thread.start_new_thread(start_server, ())
+
+    while not rospy.is_shutdown():
+        pass
+
+    kill = True
+    exit()
