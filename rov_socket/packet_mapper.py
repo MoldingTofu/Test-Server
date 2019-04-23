746f589e8cfe540aa0301018948f5b44ebd6d202
Jeremy Chang <jeremykchang@gmail.com>
2019-04-23 18:37:10 -0400

    update to current packet mapper

--- rov_socket/packet_mapper.py
+++ rov_socket/packet_mapper.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python
+#https://wiki.ros.org/rospy_message_converter ??
 import shared_msgs.msg
 import json
+import os
 
 class packet_mapper:
 
@@ -15,31 +17,45 @@ class packet_mapper:
                 if type(value).__name__ == 'member_descriptor':
                     names.append(key)
 
-        print 'names: ', names
+        #print 'names: ', names
 
         for name in names:
             path = self._build_path(packet, name)
             if path:
                 self.topic_paths[name] = path
 
-        print 'paths: ', self.topic_paths
+        #print 'paths: ', self.topic_paths
 
+    # update packet[path] = value
     def map(self, var, value, packet):
-        path = self.topic_paths[var]
+        path = self._build_path(packet, var)
+        #path = self.topic_paths[var]
         split = path.split('.')
         p = packet
         for e in split[:-1]:
             p = p[e]
         p[split[-1]] = value
 
-    def pam(self, packet, msg):
-        names = get_msg_vars(msg)
+    # update msg[path] = packet[path]
+    def pam(self, msg, packet):
+        names = self.get_msg_vars(msg)
+        for name in names:
+            path = self._build_path(packet, name)
+            #path = self.topic_paths[name]
+            split = path.split('.')
+            p = packet
+            for e in split[:-1]:
+                p = p[e]
+            setattr(msg, name, p[split[-1]])
 
     def get_msg_vars(self, msg):
         names = []
-        for key,value in getattr(shared_msgs.msg, type(msg).__name__).__dict__.iteritems():
-            if type(value).__name__ == 'member_descriptor':
-                names.append(key)
+        #for key,value in getattr(shared_msgs.msg, type(msg).__name__).__dict__.iteritems():
+        #    if type(value).__name__ == 'member_descriptor':
+        #        names.append(key)
+        #return names
+        for name, var_type in zip(msg.__slots__, msg._slot_types):
+            names.append(name)
         return names
 
     def _build_path(self, d, name):
@@ -53,23 +69,36 @@ class packet_mapper:
         return ""
 
 if __name__ == "__main__":
-    packet = { 'addr' : 'hello', 'in2' : { 'data' : 1    }   }
-    json = { 'one' : { 'two':2, 'three':3}}
+    #with open ('../../../../surface/frontend/src/packets.json') as json_data:
+    #  packet = json.load(json_data,)
+    packet = { 'lol': 1, 'meme': { 'id':'1', 'asdf': '3', 'data': '2' } }
+    #json = { 'one' : { 'two':2, 'three':3}}
     mapper = packet_mapper(packet)
 
-    for data in json['one']:
-      print data
+    #for data in json['one']:
+    #  print data
 
     # This code changes variables within a packet
-    #mapper.map('addr', '6581', packet)
-    mapper.map('data', '0', packet)
-    #print packet["in2"]["data"]
+    mapper.map('lol', '6581', packet)
+    print packet
+    #mapper.map('data', '0', packet)
 
-    # This code gives you a list of the names of the variables in a message object
+    #This code gives you a list of the names of the variables in a message object
 
-    #msg = shared_msgs.msg.can_msg()
+    #msg = shared_msgs.msg.thrust_command_msg()
+    #print
+    #lol = zip(msg.__slots__, msg._slot_types)
+    #print lol
+    #print
+    #print packet
+
+    #names = mapper.get_msg_vars(msg)
+    #for name in names:
+    #  mapper.map(name, getattr(msg, name), packet)
+    #print packet
+
+    #packet = { 'lol': 1, 'meme': { 'id':'1', 'lol': '3', 'data': '2' } }
+
+    #mapper.pam(msg, packet)
+    #print packet
     #print msg
-    #print mapper.get_msg_vars(msg)
-    #print getattr(msg, "id")
-    #setattr(msg, "id", 1)
-    #print getattr(msg, "id")
